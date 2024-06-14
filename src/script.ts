9c5d46c8f56ac5fcba92448632e6637af7b94ab0
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-14 10:58:39 +0200

    Change spy function to a counter for testing

--- src/script.ts
+++ src/script.ts
@@ -1,24 +1,30 @@
 let cachedValue: number | null = null;
 let result: number | null = null;
+let count: number = 0;
 
 function calculateDouble(n: number): number | null {
   console.log(`calculateDouble called with ${n}`);
-      if (cachedValue === n) {
-        console.log('Returning cached result');
-        return result;
-      } else {
-        console.log('Calculating new result');
-        calculate(n);
-        return result;
-      }
+  if (cachedValue === n) {
+    console.log('Returning cached result');
+    return result;
+  } else {
+    console.log('Calculating new result');
+    calculate(n);
+    return result;
+  }
 }
 
 function calculate(n: number): void {
   console.log(`calculate called with ${n}`);
-        result = 2 * n;
-        cachedValue = n;
-        console.log(result);
-        console.log(`Result is ${result}`);
+  count ++;
+  result = 2 * n;
+  cachedValue = n;
+  console.log(result);
+  console.log(`Result is ${result}`);
 }
 
-export { calculateDouble, calculate };
+function currentCount(): number {
+  return count;
+}
+
+export { calculateDouble, calculate, currentCount };
--- tests/script.test.ts
+++ tests/script.test.ts
@@ -1,17 +1,13 @@
-import * as scriptModule from '../src/script'; // Import the entire module
+import * as scriptModule from '../src/script';
 
-describe('Check if value is saved calculation is NOT called', () => {
+describe('Check if calculation function is called once', () => {
     it('should return true', () => {
-        // Spy on the calculate function to track calls
-        const calculateSpy = jest.spyOn(scriptModule, 'calculate');
 
-        scriptModule.calculateDouble(28); // First call
-        scriptModule.calculateDouble(28); // Second call
-        scriptModule.calculateDouble(28); // Third call
+        scriptModule.calculateDouble(28);
+        scriptModule.calculateDouble(28);
+        scriptModule.calculateDouble(28);
 
-        expect(calculateSpy).toHaveBeenCalledTimes(1);
+        expect(scriptModule.currentCount()).toBe(1);
 
-        // Restore the original function
-        calculateSpy.mockRestore();
     });
 });
